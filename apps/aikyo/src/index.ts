220d6ac0d9e6032fdba9861cf774dbbf89c2a1c0
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-10-09 16:26:56 +0000

    port番号を変更

--- apps/aikyo/src/index.ts
+++ apps/aikyo/src/index.ts
@@ -4,6 +4,6 @@ import { createFirehoseServer } from "./utils/firehose";
 
 (async () => {
   createCompanionServer([kyokoCompanionCard], [kyokoHistory]);
-  createFirehoseServer(8001, "companion_kyoko");
+  createFirehoseServer(8000, "companion_kyoko");
   createFirehoseServer(8080, "companion_aya");
 })();
