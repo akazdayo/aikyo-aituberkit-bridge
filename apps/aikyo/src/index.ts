3f33d3372be00d34301d7db79489996cdf7685cf
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-10-11 06:51:07 +0000

    ayaコンパニオンカードを追加し、サーバー設定を更新

--- apps/aikyo/src/index.ts
+++ apps/aikyo/src/index.ts
@@ -1,8 +1,10 @@
 import { kyokoCompanionCard, kyokoHistory } from "./cards/kyoko";
+import { ayaCompanionCard, ayaHistory } from "./cards/aya";
 import { createCompanionServer } from "./utils/companion";
 import { createFirehoseServer } from "./utils/firehose";
 
 (async () => {
-  createCompanionServer([{ agent: kyokoCompanionCard, history: kyokoHistory }], 5000);
+  createCompanionServer([{ agent: kyokoCompanionCard, history: kyokoHistory }, { agent: ayaCompanionCard, history: ayaHistory }], 5000);
   createFirehoseServer(8000, "companion_kyoko");
+  createFirehoseServer(8001, "companion_aya");
 })();
